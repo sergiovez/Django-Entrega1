1f272912fb98ca9f519ecc93bdf3900b40741ab1
root <sergiovez13@gmail.com>
2025-12-04 11:29:39 +0100

    Describe tus cambios aquí

--- my_articles_project/my_articles/settings.py
+++ my_articles_project/my_articles/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 from django.urls import reverse_lazy
 
@@ -84,11 +84,9 @@ WSGI_APPLICATION = 'my_articles.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
 
 
 # Password validation
